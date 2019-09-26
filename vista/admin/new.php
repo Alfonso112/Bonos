d09923591a4dea92409cbe44af10f20e7e6d746b
andresarroyo <50636496+andresarroyo@users.noreply.github.com>
2019-09-26 16:28:24 -0500

    Create new.php
    aqui se sube en la carpeta vista y la sub carpeta admin el archivo new

---
+++ vista/admin/new.php
@@ -0,0 +1,22 @@
+<?php
+include_once 'headAdmin.php';
+?>
+<!-- contenido-w de las cosas -->
+<div id="contenido-w" style="overflow-y: auto; height: 90vh;">
+
+    <!-- a partir de aqui se agregan cosas -->
+
+    <!-- container-fluid -->
+<div class="container-fluid">
+
+
+
+
+</div>
+    <!-- fin container-fluid-->
+</div>
+<!-- fin contenido-w de las cosas -->
+
+<?php
+
+include_once 'footerAdmin.php';
